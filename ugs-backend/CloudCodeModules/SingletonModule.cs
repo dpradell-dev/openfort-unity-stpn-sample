bb804f4660f080f68ea2d39ed77ac00a79e5b6e4
dpradell <dpradell.dev@gmail.com>
2024-02-13 02:27:21 +0100

    Update SingletonModule.cs

--- ugs-backend/CloudCodeModules/SingletonModule.cs
+++ ugs-backend/CloudCodeModules/SingletonModule.cs
@@ -8,12 +8,12 @@ public class SingletonModule
     private static SingletonModule instance;
     private static readonly object Padlock = new object();
 
-    private const string OfApiKey = "sk_test_d79a8f16-c56d-528b-8b69-40d3561a1736";
-    public const string OfNftContract = "con_dd370178-0cca-4888-a2a0-ea194a7415c7";
-    public const string OfGoldContract = "con_ef22a1b9-5ae8-49df-8a15-c7bde75c93dd";
-    public const string OfSponsorPolicy = "pol_a43e5145-6db9-4351-a800-b5c473d5dc92";
-    public const string OfDevTreasuryAccount = "dac_05a72f40-b2d6-4d91-9e40-880508527e03";
-    public const string OfDevMintingAccount = "dac_fcb38079-a918-406a-a968-672688c5fed1";
+    private const string OfApiKey = "";
+    public const string OfNftContract = "";
+    public const string OfGoldContract = "";
+    public const string OfSponsorPolicy = "";
+    public const string OfDevTreasuryAccount = "";
+    public const string OfDevMintingAccount = "";
     
     public OpenfortClient OfClient { get; private set; }
     public int ChainId { get; } = 80001;
